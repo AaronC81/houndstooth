a613e75217cdf084ffa325730c87b1b95289eaa9
Aaron Christiansen <aaronc20000@gmail.com>
2021-11-29 22:50:30 +0000

    Break up local variable code to use a mixin

--- lib/nodes/identifiers.rb
+++ lib/nodes/identifiers.rb
@@ -47,17 +47,43 @@ class ConstantAssignment < SemanticNode
     end
 end
 
+module VariableMixin
+    def variable_mixin(type)
+        # @return [Symbol]
+        attr_accessor :name
+        
+        register_ast_converter type do |ast_node|
+            self.new(ast_node: ast_node, name: ast_node.to_a.first)
+        end
+    end
+
+    def variable_assignment_mixin(type)
+        # @return [Symbol]
+        attr_accessor :name
+
+        # @return [SemanticNode]
+        attr_accessor :value
+
+        register_ast_converter type do |ast_node|
+            name, value = *ast_node
+            value = from_ast(value)
+            
+            self.new(
+                ast_node: ast_node,
+                name: name,
+                value: value,
+            )
+        end
+    end
+end
+
 class LocalVariable < SemanticNode
-    # @return [Symbol]
-    attr_accessor :name
+    extend VariableMixin
+    variable_mixin :lvar
 
     # @return [Boolean]
     attr_accessor :fabricated
     alias fabricated? fabricated
-
-    register_ast_converter :lvar do |ast_node|
-        LocalVariable.new(ast_node: ast_node, name: ast_node.to_a.first, fabricated: false)
-    end
 
     def self.fabricate
         @@fabricate_counter ||= 0
@@ -69,20 +95,6 @@ class LocalVariable < SemanticNode
 end
 
 class LocalVariableAssignment < SemanticNode
-    # @return [Symbol]
-    attr_accessor :name
-
-    # @return [SemanticNode]
-    attr_accessor :value
-
-    register_ast_converter :lvasgn do |ast_node|
-        name, value = *ast_node
-        value = from_ast(value)
-        
-        LocalVariableAssignment.new(
-            ast_node: ast_node,
-            name: name,
-            value: value,
-        )
-    end
+    extend VariableMixin
+    variable_assignment_mixin :lvasgn
 end
