497209b123187e6a0f3e9e4245eae44be8a45805
Aaron Christiansen <aaronc20000@gmail.com>
2022-01-25 14:09:55 +0000

    Make CLI use environment builder

--- lib/cli.rb
+++ lib/cli.rb
@@ -9,4 +9,20 @@ def code_to_semantic_node(code)
     TypeChecker::SemanticNode.from_ast(ast_node)
 end
 
-pp code_to_semantic_node(ARGV[0]) if __FILE__ == $0
+if __FILE__ == $0
+    node = code_to_semantic_node(File.read(ARGV[0]))
+    env = TypeChecker::Environment.new
+    TypeChecker::Stdlib.add_types(env)
+    TypeChecker::Environment::Builder.new(node, env).analyze
+
+    if TypeChecker::Errors.errors.any?
+        TypeChecker::Errors.errors.each do |error|
+            puts error.format
+            puts
+        end
+    else
+        puts "All done!"
+        puts "Types:"
+        puts env.types.keys.map { |name| "  - #{name}"}
+    end
+end
